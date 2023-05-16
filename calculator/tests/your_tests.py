37d93373300e924d2a5205268569c4d3184ccb7c
emchiao <echiao19@gmail.com>
2023-05-15 20:19:08 -0700

    Added 6 of my own tests

--- calculator/tests/your_tests.py
+++ calculator/tests/your_tests.py
@@ -4,6 +4,18 @@ from calculator_adapter import run
 
 ### ADD AT LEAST TWO TESTS HERE!
 
+# Checks that the program outputs "7" for an input of "3 + 4"
+assert run("3 + 4").output == "7"
+# Checks that the program outputs "8" for an input of "3 * 7"
+assert run("3 * 7").output == "21"
+# Checks that the program outputs "2" for an input of "10 - 8"
+assert run("10 - 8").output == "2"
+# Checks that the program outputs "4" for an input of "20 / 5"
+assert run("20 / 5").output == "4"
+# Checks that the program exists successfully (no error) for input "3 * 7"
+assert run("3 * 7").exit_status == 0
+# Checks that the program fails (correctly errors) for input "1 ! 9"
+assert run("1 ! 9").exit_status != 0
 
 ###
 
